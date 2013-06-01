ba90299785a42cbaaddff8dcf86a5f847c7ff173
Alex Grosjean <LinxuSx@gmail.com>
2013-05-31 22:54:16 -0500

    Added while loop to continue playing, length comparison of words to clean up, and attempt at opening internet file

--- Scrabble.py
+++ Scrabble.py
@@ -1,4 +1,5 @@
 import os
+#from urllib2 import urlopen
 
 # Author: Alex Grosjean
 # 31 March 2013
@@ -6,10 +7,6 @@ import os
 # returns each possible word and the value
 
 def compute_score(word):
-	#value = 0
-	#for letter in word:
-	#	value += scores[letter.lower()]
-	#return value
 	return sum(scores[c.lower()] for c in word)
 
 scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
@@ -23,60 +20,73 @@ valid_list = []
 usedRack = []
 
 with open("C:\Users\AlexGrosjean\Documents\Python\Supplements\sowpods\sowpods.txt","r") as wordDoc:
-	for word in wordDoc.readlines():
+	for word in wordDoc:
 		word = word.strip('\n')
 		wordList.append(word)
-	
-myRack = raw_input("Please enter your word: ")
-myRack = myRack.upper()
-print "...validating word against dictionary and computing values..."
 
-for dictWord in wordList:
-	#AB
-	usedRack = []
-	for letter in myRack:
-		usedRack.append(letter)
-	myWord = ""
-	
-	for dictLetter in dictWord:
-		#A
-		#B
-		index = 0
-		for myLetter in usedRack:
-			#A
-			#B
-			#B
-			#A
-			if len(usedRack) > 0:
-				if dictLetter == myLetter:
-					myWord += dictLetter
-					del(usedRack[index])
-					break
-			index += 1
+print "Hello and welcome to the scrabble cheating engine!"
 		
-		if myWord == dictWord:
-			valid_list.append(myWord)
-			break
+continueScrabble = True
+while continueScrabble:
+		
+	myRack = raw_input("Please enter your word: ")
+	myRack = myRack.upper()
+	print "...validating word against dictionary and computing values..."
+
+	for dictWord in wordList:
+		#AB
+		usedRack = []
+		for letter in myRack:
+			usedRack.append(letter)
+		myWord = ""
+		
+		if len(dictWord) <= len(myRack):
+			
+			for dictLetter in dictWord:
+				#A
+				#B
+				index = 0
+				for myLetter in usedRack:
+					#A
+					#B
+					#B
+					#A
+					if len(usedRack) > 0:
+						if dictLetter == myLetter:
+							myWord += dictLetter
+							del(usedRack[index])
+							break
+					index += 1
+				
+				if myWord == dictWord:
+					valid_list.append(myWord)
+					break
 
 
-wordListScore = {}
-	
-for word in valid_list:
-	wordListScore[word] = compute_score(word)
-	
-if len(valid_list) > 50:
-	print "...printing word to word document..."
+	wordListScore = {}
+		
+	for word in valid_list:
+		wordListScore[word] = compute_score(word)
+		
+	if len(valid_list) > 50:
+		print "...printing word to word document..."
 
-	fileLocation = os.path.join(r"C:\Users\AlexGrosjean\Documents\Python\Supplements\sowpods",myRack.title()+"WordList")
-	fileLocation += ".txt"
-	
-	with open(fileLocation,"w") as wordDoc:
-		for word in wordListScore:
-			wordDoc.write(word + " : " + str(wordListScore[word]) + "\n")
-	print "Complete! Your words have been printed to: %s%s " % (myRack.title(),"WordList")
+		fileLocation = os.path.join(r"C:\Users\AlexGrosjean\Documents\Python\Supplements\sowpods",myRack.title()+"WordList")
+		fileLocation += ".txt"
+		
+		with open(fileLocation,"w") as wordDoc:
+			for word in wordListScore:
+				wordDoc.write(word + " : " + str(wordListScore[word]) + "\n")
+		print "Complete! Your words have been printed to: %s%s " % (myRack.title(),"WordList")
+	else:
+		print "There are %i words in your list." % len(valid_list)
+		for word in sorted(wordListScore):
+			print word + " : " + str(wordListScore[word])
+
+	print "The largest value word in your rack is: %s : %i" % (max(wordListScore, key=wordListScore.get),wordListScore[max(wordListScore, key=wordListScore.get)])
+	response = raw_input("Would you like to play again? (y \ n) : ")
+		
+	if response[0].lower() != "y":
+		continueScrabble = False
 else:
-	print "There are %i words in your list." % len(valid_list)
-	for word in sorted(wordListScore):
-		print word + " : " + str(wordListScore[word])
-
-print "The largest value word in your rack is: %s : %i" % (max(wordListScore, key=wordListScore.get),wordListScore[max(wordListScore, key=wordListScore.get)])
+	print "Thanks for playing!"
